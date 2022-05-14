6e747594d9266830e02540eb2d0d5033d80b7ce0
rahul unni <rahul@rahulsmac.local>
2022-05-14 16:17:17 +0530

    UC1 added

--- UC1.cs
+++ UC1.cs
@@ -3,8 +3,21 @@ namespace EmployeeWageComp
 {
     public class UC1
     {
-        public UC1()
+        public void UsCa1()
         {
+            
+            const int isPresent = 1;
+            Random random = new Random(); 
+            int check = random.Next(0, 2);
+            if (check == isPresent) 
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
 }
