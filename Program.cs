6464883e05f6d8813f882d20d1e6968f70034f2c
rahul unni <rahul@rahulsmac.local>
2022-05-14 16:14:44 +0530

    Program added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeWageComp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            UC1 usc1 = new UC1();
+            usc1.UsCa1();
+        }
+    }
+}
---
+++ UC1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace EmployeeWageComp
+{
+    public class UC1
+    {
+        public UC1()
+        {
+        }
+    }
+}
