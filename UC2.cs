7f4d16ef95e0280c0de4626f8c2ca8e8c9f4f482
rahul unni <rahul@rahulsmac.local>
2022-05-14 16:56:02 +0530

    UC2 added

---
+++ UC2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace EmployeeWageComp
+{
+    public class UC2
+    {
+        public void UsCa2()
+        {
+            Random random = new Random();
+            int is_fulltime = 1;
+            int totalWorkingHour = 8;
+            int wagePerHour = 20;
+            int dailyWage = 0;
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == is_fulltime)
+            {
+                Console.WriteLine("Employee is Present");
+                dailyWage = totalWorkingHour * wagePerHour;
+                Console.WriteLine("Total wage of a day: " + dailyWage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                dailyWage = totalWorkingHour * 0;
+                Console.WriteLine("Total wage of a day: " + dailyWage);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
