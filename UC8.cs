2387ffafa7e187214ee9259651fea52c68871a3d
rahul unni <rahul@rahulsmac.local>
2022-05-14 17:32:20 +0530

    UC8 added

---
+++ UC8.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace EmployeeWageComp
+{
+    public class UC8
+    {
+
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            public static void Usca8(string companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) 
+            {
+                int totalMonthWage = 0, empWorkHour = 0, empDailyWage = 0, empTotalHour = 0, empTotalWorkDays = 0;
+                Random random = new Random(); 
+                while (empTotalHour <= maxWorkingHours && empTotalWorkDays <= maxWorkingDays) 
+                {
+                    int check = random.Next(0, 3); 
+                                                   
+                    switch (check)
+                    {
+                        case IS_FULL_TIME:                           
+                            empWorkHour = 8;
+                            break;
+
+                        case IS_PART_TIME:
+                            empWorkHour = 4;
+                            break;
+                        default:
+                            empWorkHour = 0;
+                            break;
+                    }
+                    empDailyWage = empWorkHour * wagePerHour; 
+                                                               
+                    totalMonthWage += empDailyWage; 
+                    empTotalWorkDays++;
+                    empTotalHour += empWorkHour;
+                }
+                if (empTotalHour > maxWorkingHours) 
+                {
+                    int a = empTotalHour - maxWorkingHours;
+                    empTotalHour -= a;
+                    int wage = a * wagePerHour; 
+                    totalMonthWage -= wage; 
+                }
+                if (empTotalWorkDays > maxWorkingDays)
+                {
+                    empTotalWorkDays -= 1;
+                }
+                Console.WriteLine("\n\nCompany Name : " + companyName);
+                Console.WriteLine($"\nEmployee total working days   : {empTotalWorkDays}");
+                Console.WriteLine($"Employee total working hours  : {empTotalHour}\n");
+                Console.WriteLine($"Employee Total Month Wage : {totalMonthWage}\n");
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Employee Wage Compuatation Program\n");
+                Usca8("Bridgelabz", 20, 20, 100);
+                Usca8("Infosys", 40, 20, 100);
+                Console.ReadLine();
+            }
+        
+    }
+}
